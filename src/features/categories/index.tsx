bbdb7bdd3b9c66e0a529c0c5c6ac41b76ccc5031
Joshua Richardson <jrichardson@linear-systems.com>
2022-01-28 10:14:15 -0800

    Category now added successfully

--- src/features/categories/index.tsx
+++ src/features/categories/index.tsx
@@ -1,22 +1,29 @@
 // react
 import { FC } from 'react'
-import { useSelector } from '../../hooks/useRedux'
 
 // modules
-
 // project files
+import { useDispatch, useSelector } from '../../hooks/useRedux'
+import { createCategory } from './categorySlice'
 import Category from './components/Category'
 
 type Props = {}
 const Content: FC<Props> = () => {
+  const dispatch = useDispatch()
   const { activeTab, list, order } = useSelector((state) => state.tabs)
+  const addCategory = () => {
+    const newCategory = { newTitle: 'New Category', tabId: order[activeTab] }
+    dispatch(createCategory(newCategory))
+  }
 
   return (
     <main>
       {list[order[activeTab]].categories.map((categoryId) => (
         <Category key={categoryId} id={categoryId} />
       ))}
-      <button className='fab'>+</button>
+      <button className='fab' onClick={addCategory}>
+        +
+      </button>
     </main>
   )
 }

2c4476870349f9e4a5af217da04897ece7cc74c9
Joshua Richardson <jrichardson@linear-systems.com>
2022-01-28 17:14:50 -0800

    Add logic for opening all bookmarks in a category

--- src/features/categories/components/CardHeader.tsx
+++ src/features/categories/components/CardHeader.tsx
@@ -4,16 +4,29 @@ import { FC, useState } from 'react'
 // modules
 // project files
 import { ReactComponent as EditIcon } from '../../../assets/editIcon.svg'
+import { ReactComponent as OpenAllLinks } from '../../../assets/externalLink.svg'
+import { useSelector } from '../../../hooks/useRedux'
 import CardTitle from './CardTitle'
 
 type Props = { id: string; title: string }
 const CardHeader: FC<Props> = ({ id, title }) => {
+  const bookmarks = useSelector((state) => state.bookmarks)
+  const { [id]: thisCategory } = useSelector((state) => state.categories)
   const [showEditButton, setShowEditButton] = useState(false)
   const [inEditMode, setInEditMode] = useState(false)
 
   const handleMouseOver = () => setShowEditButton(true)
   const handleMouseOut = () => setShowEditButton(false)
   const toggleEditMode = () => setInEditMode((prev) => !prev)
+  const openAllLinks = () => {
+    thisCategory.bookmarks.forEach((bookmark) => {
+      // TODO: Need to add logic to check for allow-popups from site
+      // Modern browsers don't allow multiple opens by default
+      // Need to see if I can check if popups allowed, and catch if not
+      const { url } = bookmarks[bookmark]
+      window.open(url, '_blank')
+    })
+  }
 
   return (
     <div
@@ -23,9 +36,14 @@ const CardHeader: FC<Props> = ({ id, title }) => {
     >
       <CardTitle id={id} inEditMode={inEditMode} title={title} />
       {showEditButton && (
-        <button className='edit-button' onClick={toggleEditMode}>
-          <EditIcon />
-        </button>
+        <>
+          <button className='edit-button' onClick={toggleEditMode}>
+            <EditIcon />
+          </button>
+          <button className='open-all-button' onClick={openAllLinks}>
+            <OpenAllLinks />
+          </button>
+        </>
       )}
     </div>
   )
